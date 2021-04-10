f60600c404714bf092b61bd6faea35a433de5fd2
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-04-10 22:03:07 +0500

    wip to be continued

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,9 @@ export default function Home() {
 {/* body */}
 <form action="">
   <Image
-  src="https://google.com/"
+  width="5px"
+  height="100%"
+  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
   />
 </form>
 
